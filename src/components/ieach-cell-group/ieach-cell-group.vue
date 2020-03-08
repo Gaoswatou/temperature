1b1d8dda0d2b6146c3076563bc5bf561130c2ae5
someone <719617315@qq.com>
2020-03-08 20:23:11 +0800

    cell-groud组件

---
+++ src/components/ieach-cell-group/ieach-cell-group.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="i-cell-group">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "ieach-cell-group"
+};
+</script>
+<style lang="scss" scope>
+.i-cell-group {
+  border-top: 1px solid #e8e8e8;
+  border-bottom: 1px solid #e8e8e8;
+}
+</style>
