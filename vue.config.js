7fa5d34ce965b41fc59c3adf493c1cc5643d2f40
someone <719617315@qq.com>
2020-03-10 21:45:55 +0800

    自适应配置

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,19 @@
+const autoprefixer = require("autoprefixer");
+const pxtorem = require("postcss-pxtorem");
 module.exports = {
   publicPath: process.env.NODE_ENV === "./" ? "/mobile/" : "./",
   css: {
-    loaderOptions: {
-      css: {},
-      postcss: {
-        plugins: [
-          require("postcss-px2rem")({
-            remUnit: 37.5
-          })
-        ]
+    css: {
+      loaderOptions: {
+        postcss: {
+          plugins: [
+            autoprefixer(),
+            pxtorem({
+              rootValue: 75,
+              propList: ["*"]
+            })
+          ]
+        }
       }
     }
   }
